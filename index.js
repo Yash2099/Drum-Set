f8a0beed1c22f242ff5fe0c4cb8b8978d6d10f21
Yashasvi Baranwal <110628058+Yash2099@users.noreply.github.com>
2023-04-13 11:16:02 +0530

    Updated the animation part

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ for(var i=0;i<numberOfDrumButtons; i++)
   document.querySelectorAll(".drum")[i].addEventListener("click",function() {
    var buttonInnerHTML = this.innerHTML;
    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
 
   });
 }
@@ -56,6 +57,14 @@ function makeSound(key)
     default: console.log(buttonInnerHTML);
 
   }
+}
+function buttonAnimation(currentKey)
+{
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  },100);
 }
 
 
